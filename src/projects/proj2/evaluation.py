a165439b22c9f3e82b6f94dbcb500bdfde9f839a
Mikey Ferguson <mikeytallyferguson@gmail.com>
2024-10-29 20:17:18 -0500

    began refactor of evaluation

--- src/projects/proj2/evaluation.py
+++ src/projects/proj2/evaluation.py
@@ -3,8 +3,8 @@
 import math, random
 from ...lib.game import discrete_soccer
 
-BLUE_WIN = 500
-RED_WIN = -500
+RED_WIN = 500
+BLUE_WIN = -500
 
 def soccer(state, player_id):
     # The soccer evaluation function *must* look into the game state
@@ -19,9 +19,9 @@ def soccer(state, player_id):
     if not isinstance(state, discrete_soccer.SoccerState):
         raise ValueError("Evaluation function incompatible with game type.")
 
-    # We're going to evaluate the state of the player from the point of view of the blue player.
-    # That is, if the blue player is the winner given this state, we'll return a super big POSITIVE number.
-    # If the red player is the winner given this state, we'll return a super big NEGATIVE number.
+    # We're going to evaluate the state of the player from the point of view of the red player.
+    # That is, if the red player is the winner given this state, we'll return a super big POSITIVE number.
+    # If the blue player is the winner given this state, we'll return a super big NEGATIVE number.
     # Otherwise, we'll need to dive into the game state and decide which player is at an advantage
     if state.reward(player_id=player_id) == 10:
         # Blue won
@@ -34,51 +34,17 @@ def soccer(state, player_id):
         # The game is still ongoing
 
         # See who is in possession of the ball
-        if state.current_player_obj.team == discrete_soccer.Team.BLUE:
-            # Blue is happy to get to go
-            total += 10
-        else:
-            # Red is happy to get to go
-            total -= 10
+        red_in_possession = False
+        if state.current_player_obj.team == discrete_soccer.Team.RED:
+            red_in_possession = True
         
         # See where the ball is and whose goal the ball is closer to
         x_ball = state.ball.x
         y_ball = state.ball.y
+        x_red = state.players[0].x
+        y_red = state.players[0].y
+        x_blue = state.players[1].x
+        y_blue = state.players[1].y
         level_with_goal = state.goal_bottom <= y_ball and y_ball <= state.goal_top
-        # If the ball is closer to a certain goal AND level to be kicked into said goal, then SOMEONE is going to be really happy with the state
-        ball_score_multiplier = 5 if level_with_goal else 1
-        if abs(state.red_goal_pos[0] - x_ball) < abs(state.blue_goal_pos[0] - x_ball):
-            # Ball is closer to the red goal than the blue goal, which makes the blue team happier the closer the ball is to the red goal
-            total += abs(state.blue_goal_pos[0] - x_ball) * ball_score_multiplier
-        elif abs(state.red_goal_pos[0] - x_ball) > abs(state.blue_goal_pos[0] - x_ball):
-            # Ball is closer to the blue goal, which makes the red ream happier the closer the ball is to the blue goal
-            total -= abs(state.red_goal_pos[0] - x_ball) * ball_score_multiplier
-
-        # Now we should consider who is closer to being in possession of the ball
-        if state.current_player_obj.team == discrete_soccer.Team.BLUE and state.current_player_obj.has_ball:
-            # Blue team is quite happy to have the ball
-            total += 75
-        elif state.current_player_obj.team == discrete_soccer.Team.RED and state.current_player_obj.has_ball:
-            # Red team is quite happy to have the ball
-            total -= 75
-        else:
-            # See who is closer to the ball (Manhattan Distance)
-            red_x = state.players[player_id].x \
-                if state.players[player_id].team == discrete_soccer.Team.RED \
-                else state.players[(player_id + 1) % 2].x
-            blue_x = state.players[player_id].x \
-                if state.players[player_id].team == discrete_soccer.Team.BLUE \
-                else state.players[(player_id + 1) % 2].x
-            red_y = state.players[player_id].y \
-                if state.players[player_id].team == discrete_soccer.Team.RED \
-                else state.players[(player_id + 1) % 2].y
-            blue_y = state.players[player_id].y \
-                if state.players[player_id].team == discrete_soccer.Team.BLUE \
-                else state.players[(player_id + 1) % 2].y
-            
-            red_distance = abs(x_ball - red_x) + abs(y_ball - red_y)
-            blue_distance = abs(x_ball - blue_x) + abs(y_ball - blue_y)
-            # Score increases if blue is closer, and score decreases if red is closer
-            total += 10 * (red_distance - blue_distance)
-
-        return -total
+        
+        return total
